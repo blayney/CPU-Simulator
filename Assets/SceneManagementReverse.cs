82c7c70c7e2a5b15de1f2f6dac89180aed376e7e
Will Blayney <woblayney@gmail.com>
2019-11-05 22:52:04 +0000

    Fixed build errors and added resets

---
+++ Assets/SceneManagementReverse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SceneManagementReverse : MonoBehaviour
+{
+    public Slider Slidy;
+    public Button SwitchButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchButton.onClick.AddListener(delegate { LoadLevel(0); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+ 
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(0));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(0);
+        while (!op.isDone)
+        {
+            float progress = Mathf.Clamp01(op.progress / .9f);
+            Slidy.value = progress;
+            yield return null;
+        }
+       
+
+    }
+}
