7ebaa99aef749301ffd846ffefae57823f123fed
Will Blayney <woblayney@gmail.com>
2019-11-05 17:54:31 +0000

    Added main menu and slider

---
+++ Assets/SceneManagement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SceneManagement : MonoBehaviour
+{
+    public Slider Slidy;
+    public Button SwitchButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchButton.onClick.AddListener(delegate { LoadLevel(1); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+ 
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(1));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!op.isDone)
+        {
+            float progress = Mathf.Clamp01(op.progress / .9f);
+            Slidy.value = progress;
+            yield return null;
+        }
+
+
+    }
